ac7fec403c3dcc5b60629edb577319c1325ee423
MarwanAshraf03 <marwanashraf556.study@gmail.com>
2025-08-11 20:22:39 +0300

    update

--- file.js
+++ file.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 
 const filePath = "~/storage/documents/example.txt";
+if (fs.existsSync(filePath)) {
+  console.log("File exists");
+} else {
+  console.log("File does not exist, creating directory...");
+  fs.mkdirSync(filePath, { recursive: true });
+  console.log("Directory created");
+}
 fs.writeFileSync(filePath, "Hello, world!", (err) => {
   if (err) {
     console.error("Error writing file:", err);
